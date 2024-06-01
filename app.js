542d50a303992b019852e9e140b865b396f017f5
samuelribeiroo <samuelribeirodev@gmail.com>
2024-05-31 23:09:35 -0300

    add(feat): show characters from DB

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 import * as characters from "./api/database.js";
 
+const $containerCharacters = document.querySelector(".characters-container");
+let listCharacters = characters.charactersList;
+
+function mountCharactersComponent(character) {
+  const charactersName = document.createElement("h1");
+  charactersName.textContent = character.name;
+  $containerCharacters.appendChild(charactersName);
+
+  const charactersImage = document.createElement("img");
+  charactersImage.src = character.image;
+  charactersImage.style.width = "400px";
+  charactersImage.style.height = "400px";
+  $containerCharacters.appendChild(charactersImage);
+
+  document.body.appendChild($containerCharacters);
+
+  return { charactersName, charactersImage };
+}
+
+const displayCharacterUI = () => listCharacters.forEach(mountCharactersComponent);
+
+displayCharacterUI();
