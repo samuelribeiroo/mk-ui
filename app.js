c3092150d88bf1dc8425650f2c8995f923c2f923
samuelribeiroo <samuelribeirodev@gmail.com>
2024-06-02 18:48:25 -0300

    add: wrapper dynamically to show data from JSON

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
 import * as characters from "./api/database.js";
 
 const $containerCharacters = document.querySelector(".characters-container");
+$containerCharacters.classList.add(".characters-container");
+
+const $mainContainer = document.querySelector(".main-container");
+$mainContainer.classList.add(".main-container");
+
 let listCharacters = characters.charactersList;
 
 function mountCharactersComponent(character) {
-  const charactersName = document.createElement("h1");
-  charactersName.textContent = character.name;
-  $containerCharacters.appendChild(charactersName);
+  const boxWrapper = document.createElement("div");
+  boxWrapper.style.display = "flex";
+  boxWrapper.style.flexDirection = "column";
+  boxWrapper.style.alignItems = "center";
 
   const charactersImage = document.createElement("img");
   charactersImage.src = character.image;
-  charactersImage.style.width = "400px";
-  charactersImage.style.height = "400px";
+  charactersImage.style.width = "195px";
+  charactersImage.style.height = "235px";
+
+  const showCharacterName = document.createElement("button");
+  showCharacterName.textContent = character.name;
+  showCharacterName.style.width = "108px";
+  showCharacterName.style.height = "48px";
+  showCharacterName.style.marginTop = "32px";
+  showCharacterName.classList.add("display-name");
+
   $containerCharacters.appendChild(charactersImage);
+  charactersImage.after(showCharacterName);
+  boxWrapper.appendChild(charactersImage);
+  boxWrapper.appendChild(showCharacterName);
+  $containerCharacters.appendChild(boxWrapper);
+  $mainContainer.appendChild($containerCharacters);
 
-  document.body.appendChild($containerCharacters);
-
-  return { charactersName, charactersImage };
+  return { charactersImage };
 }
 
-const displayCharacterUI = () => listCharacters.forEach(mountCharactersComponent);
+const displayCharactersUI = () => listCharacters.forEach(mountCharactersComponent);
 
-displayCharacterUI();
+displayCharactersUI();
